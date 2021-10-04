243a6b07a249333920656cb36704ff148215da00
spanfilo <91565751+spanfilo@users.noreply.github.com>
2021-10-04 15:02:35 -0500

    Add files via upload

---
+++ pp.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+she=turtle.Turtle
+
+#funcions
+def randomDot():
+    she.color("blue")
+    she.begin_fill()
+    she.circle(60)
+    she.end_fill()
+#Main
+randomDot(1)
+        
